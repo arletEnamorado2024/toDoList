6384dba9bf58fa2cf2de0f7f5243fe9007c0eb19
Arlet Enamorado <arlet.martinez2024@hilasal.superate.org.sv>
2023-10-27 22:11:37 -0600

    Funcionalidad terminada y diseño añadido

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    // Método para completar la tarea
+    complete() {
+        this.completed = true;
+    }
+
+    // Método para descompletar la tarea
+    uncomplete() {
+        this.completed = false;
+    }
+}
+
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
+const tasks = []; // Array para almacenar las tareas
 
 taskForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -12,16 +31,38 @@ taskForm.addEventListener("submit", function (e) {
         return;
     }
 
+    const newTask = new Task(taskTitle, taskDescription);
+    tasks.push(newTask);
+
+    // Crear un elemento de lista para la tarea
     const taskItem = document.createElement("li");
     taskItem.innerHTML = `
-        <h3>${taskTitle}</h3>
-        <p>${taskDescription}</p>
+        <h3>${newTask.title}</h3>
+        <p>${newTask.description}</p>
         <button class="complete-button">Completar</button>
         <button class="delete-button">Eliminar</button>
     `;
 
+    // Agregar la tarea a la lista
     taskList.appendChild(taskItem);
 
     // Limpiar el formulario
     taskForm.reset();
+
+    // Agregar event listeners a los botones de completar y eliminar
+    const completeButton = taskItem.querySelector(".complete-button");
+    const deleteButton = taskItem.querySelector(".delete-button");
+
+    completeButton.addEventListener("click", () => {
+        newTask.complete();
+        // Puedes realizar acciones adicionales aquí, como cambiar el estilo de la tarea completada.
+    });
+
+    deleteButton.addEventListener("click", () => {
+        const taskIndex = tasks.indexOf(newTask);
+        if (taskIndex !== -1) {
+            tasks.splice(taskIndex, 1);
+        }
+        taskList.removeChild(taskItem);
+    });
 });
